b0d1a34ab303c5d8dfb75f4b11dc899ac7d3b12d
Nehreet Kaur <kaur.nehreet@gmail.com>
2020-12-29 23:51:19 -0600

    pushing my code

---
+++ Develop/server.js
@@ -0,0 +1,116 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+let dbJSON = require("./db/db.json");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get('/notes', function(req,res){
+  //we watn to send the notes.html file
+  console.log('/NOTES ROUTE RECEIVED A REQUEST')
+  res.sendFile(path.join(__dirname,"/public/notes.html"));
+
+ })
+
+
+// api.get('/api/ notes',function(req,res){
+//fs.readFile("/db.json","utf8",function(error,data){
+//res.json(JSON.parse(data));
+
+//});
+//});
+
+app.get("/api/notes", function (req, res) {
+  res.json(dbJSON);
+});
+
+app.post("/api/notes", function (req, res) {
+  // Validate request body
+  if (!req.body.title) {
+    return res.json({ error: "Missing required title" });
+  }
+
+  // Copy request body and generate ID
+  const note = { ...req.body, id: uuidv4() }
+
+  // Push note to dbJSON array - saves data in memory
+  dbJSON.push(note);
+
+  //for delete
+
+  // app.delete("/notes/:id",function (req,res){
+  //   fs.readFile("db.json","utf8",function(error,data){
+  //     let newId=req.params.id;
+  //     let newresponse=JSON.parse(data);
+  //     newresponse=newresponse.filter
+
+  //   }
+  // }
+
+  // Saves data to file by persisting in memory variable dbJSON to db.json file.
+  // This is needed because when we turn off server we loose all memory data like pbJSON variable.
+  // Saving to file allows us to read previous notes (before server was shutdown) from file.
+  fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(dbJSON), (err) => {
+    if (err) {
+      return res.json({ error: "Error writing to file" });
+    }
+
+    return res.json(note);
+  });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  //fs.readFile("db.json","utf8",function(error,data){
+  let newId = req.params.id;
+  // dbJSON=JSON.parse(data);
+  dbJSON = dbJSON.filter(function (note) {
+    if (newId !== note.id) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  });
+  console.log(dbJSON);
+  fs.writeFile("db/db.json", JSON.stringify(dbJSON), function (error) {
+    if (error)
+      throw error;
+    return res.json(true);
+  });
+});
+
+
+
+
+app.get("*", function (req, res) {
+  res.send('<h1>Star Route</<h1>');
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# note-takerapphmk
+# About
+Made an **Note-Taker** application with 
+**node js** and **Express** and also deployed 
+
+it on **Heroku**.
+
+
+
+# TABLE OF CONTENTS
+
+[Discription](#Discription)
+
+[OUTPUT](#Output)
+
+[INSTALLATION](#Installation-requirements)
+
+[TEXT-EDITOR](#TEXT-EDITOR)
+
+[PROGRAMMING-LANGUAGES](#PROGRAMMING-LANGUAGES)
+
+[RESOURCES-REFERRED](#RESOURCES-REFERRED)
+
+[GITHUB-REPOSITORY](#GIT-HUB-REPOSITORY)
+
+[HEROKU-DEPLOYED-APPLICATION](#HEROKU-DEPLOYED-APPLICATION)
+
+
+
+
+
+
+
+# Discription
+
+In this application, user can click on **Get started**
+button. Then,user can enter the title and some text in the 
+next line. As, soon user can enter text ,**Save** button
+will be visilble. Afterwords, user can save their entry by
+clicking on the save button. Saved entry text will be visible
+on the screen. User, can delete the text entries if they want to
+by clicking on the **Red trash icon** on the screen.Also, there is
+creation of **server** and working with them.
+
+
+# Output
+
+**Note-Taker** application , in which user can **enter** 
+and **delete** notes as they like.
+
+# Installation-requirements
+
+-  **Node-js**
+- **express**
+- **Account on **HEROKU**** for
+    application deployement.
+
+## TEXT-EDITOR
+
+    Vs-code
+
+## PROGRAMMING-LANGUAGES
+
+- **JAVASCRIPT**
+- **HTML**
+- **CSS**
+
+## RESOURCES-REFERRED
+
+[FOR-NODE-EXPRESS]()
+
+[FOR-FILTER-ARRAY-FUNCTION]()
+
+## GIT-HUB-REPOSITORY
+[GITHUB-REPOSITORY]()
+
+## HEROKU-DEPLOYED-APPLICATION
+[HEROKU-DEPLOYED-APPLICATION]()
